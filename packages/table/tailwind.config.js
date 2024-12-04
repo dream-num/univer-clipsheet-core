5a37fae8d0833d2ab38259de2a46923995bbac37
siam <siam1103052133@gmail.com>
2024-12-04 14:29:50 +0800

    chore: build config

--- packages/table/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require('@univer-clipsheet-core/tailwindcss-config');
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    ...baseConfig,
-    content: ['./components/**/*.{js,ts,jsx,tsx}'],
-};
